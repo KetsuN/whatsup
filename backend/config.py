bbacf7decaa0a4f5abd77ca2295827861d1ffd7a
Tim Butler <ketsuniku@gmail.com>
2018-06-15 14:28:02 -0400

    Delete config.py

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DEBUG = True
-REDIS_HOSTNAME = os.environ.get("REDIS_HOSTNAME")
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
-REDIS_PORT = os.environ.get("REDIS_PORT")
-API_KEY = os.environ.get("API_KEY")
